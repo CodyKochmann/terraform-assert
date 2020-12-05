348558686147ff7ef6cce85b680cf11af88af10f
Cody Kochmann <kochmanncody@gmail.com>
2020-12-05 09:35:44 -0700

    updated the error to be raised for the default assertion usage

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.9.0"
+  required_version = ">= 0.12.0"
 }
 
 resource "null_resource" "checker" {
   provisioner "local-exec" {
-    command = "echo 'Condition \"${var.condition}\" failed' && exit 1"
+    command = "echo 'ASSERTION ERROR - ${var.message}' && exit 1"
   }
 
   count = "${var.condition ? 0 : 1}"
