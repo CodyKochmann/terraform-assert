ef2c0be42df2caaa18f41c37a03780300f4fc20d
Cody Kochmann <kochmanncody@gmail.com>
2020-12-05 09:30:55 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "condition" {
   description = "Boolean condition which is assumed to be true"
 }
+
+variable "message" {
+  description = "Message that will be posted if the test fails"
+}
