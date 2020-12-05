f438cdfe52c89419fdbcb37b73f49c8daabb109f
Cody Kochmann <kochmanncody@gmail.com>
2020-12-05 09:17:12 -0700

    fixed ge's interpolation syntax

--- modules/ge/main.tf
+++ modules/ge/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.9.0"
+  required_version = ">= 0.12.0"
 }
 
 resource "null_resource" "checker" {
@@ -7,5 +7,5 @@ resource "null_resource" "checker" {
     command = "echo 'Condition failed. Actual: \"${var.actual}\" expected to be greater or equal than \"${var.expected}\"' && exit 1"
   }
 
-  count = "${var.actual >= var.expected ? 0 : 1}"
+  count = var.actual >= var.expected ? 0 : 1
 }
