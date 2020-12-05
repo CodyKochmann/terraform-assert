4d0403b0aa23f34579f81cadf82fc20a32b8e1ed
Cody Kochmann <kochmanncody@gmail.com>
2020-12-05 10:34:42 -0700

    added outputs to the module so this module can enforce order with tests

---
+++ eq/outputs.tf
@@ -0,0 +1,24 @@
+# this provides an output value for other modules to "depend on"
+# to get this functionality add any of the following to any resource
+# that needs to be blocked by your assertion's failure.
+#
+#   depends_on = [module.test_name.ok]
+#   depends_on = [module.test_name.result]
+#   depends_on = [module.test_name.success]
+#
+
+output "ok" {
+  value = true
+}
+
+output "result" {
+  value = true
+}
+
+output "success" {
+  value = true
+}
+
+# Note: all of these outputs will always be available
+# to preserve compatability :)
+
