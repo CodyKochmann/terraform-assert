b47aaacf4010cd1a1e3c3f9872ed164428e621fe
Cody Kochmann <kochmanncody@gmail.com>
2020-12-05 09:29:33 -0700

    marked up the tests to reflect the new lib

--- examples/full/main.tf
+++ examples/full/main.tf
@@ -1,52 +1,53 @@
-module "assert-conditional" {
-  source    = "kchugalinskiy/assert/null"
-  condition = "${1 != 2}"
+module "assert-boolean" {
+  source    = "github.com/CodyKochmann/terraform-null-assert"
+  condition = 1 != 2
+  message   = "this really should have worked"
 }
 
 module "assert-equality" {
-  source   = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/eq"
+  source   = "git::https://github.com/CodyKochmann/terraform-null-assert.git//modules/eq"
   expected = 1
   actual   = 1
 }
 
 module "assert-equality-strings" {
-  source   = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/eq"
+  source   = "git::https://github.com/CodyKochmann/terraform-null-assert.git//modules/eq"
   expected = "hello world"
   actual   = "hello world"
 }
 
 module "assert-unequality" {
-  source     = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/neq"
+  source   = "git::https://github.com/CodyKochmann/terraform-null-assert.git//modules/neq"
   unexpected = "hello world"
   actual     = "hello"
 }
 
 module "assert-greater-or-equal" {
-  source   = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/ge"
+  source   = "git::https://github.com/CodyKochmann/terraform-null-assert.git//modules/ge"
   expected = 0
   actual   = 2
 }
 
 module "assert-strict-greater" {
-  source   = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/gt"
+  source   = "git::https://github.com/CodyKochmann/terraform-null-assert.git//modules/gt"
   expected = 0
   actual   = 1
 }
 
 module "assert-less-or-equal" {
-  source   = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/le"
+  source   = "git::https://github.com/CodyKochmann/terraform-null-assert.git//modules/le"
   expected = 2
   actual   = 0
 }
 
 module "assert-strict-less" {
-  source   = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/lt"
+  source   = "git::https://github.com/CodyKochmann/terraform-null-assert.git//modules/lt"
   expected = 1
   actual   = 0
 }
 
 module "assert-match-regex" {
-  source = "git::git@github.com:kchugalinskiy/terraform-null-assert.git//modules/match"
+  source   = "git::https://github.com/CodyKochmann/terraform-null-assert.git//modules/match"
   regex  = "[a-z]*"
   actual = "rego"
 }
