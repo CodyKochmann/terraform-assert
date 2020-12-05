cc033da08577d1557638ae68256486f152855e19
CodyKochmann <kochmanncody@gmail.com>
2020-12-05 10:11:40 -0700

    cleaned up the imports once things were working

--- examples/full/main.tf
+++ examples/full/main.tf
@@ -5,49 +5,49 @@ module "assert-boolean" {
 }
 
 module "assert-equality" {
-  source   = "git::https://github.com/CodyKochmann/terraform-assert.git//modules/eq"
+  source   = "github.com/CodyKochmann/terraform-assert/eq"
   expected = 1
   actual   = 1
 }
 
 module "assert-equality-strings" {
-  source   = "git::https://github.com/CodyKochmann/terraform-assert.git//modules/eq"
+  source   = "github.com/CodyKochmann/terraform-assert/eq"
   expected = "hello world"
   actual   = "hello world"
 }
 
 module "assert-unequality" {
-  source   = "git::https://github.com/CodyKochmann/terraform-assert.git//modules/neq"
+  source   = "github.com/CodyKochmann/terraform-assert/neq"
   unexpected = "hello world"
   actual     = "hello"
 }
 
 module "assert-greater-or-equal" {
-  source   = "git::https://github.com/CodyKochmann/terraform-assert.git//modules/ge"
+  source   = "github.com/CodyKochmann/terraform-assert/ge"
   expected = 0
   actual   = 2
 }
 
 module "assert-strict-greater" {
-  source   = "git::https://github.com/CodyKochmann/terraform-assert.git//modules/gt"
+  source   = "github.com/CodyKochmann/terraform-assert/gt"
   expected = 0
   actual   = 1
 }
 
 module "assert-less-or-equal" {
-  source   = "git::https://github.com/CodyKochmann/terraform-assert.git//modules/le"
+  source   = "github.com/CodyKochmann/terraform-assert/le"
   expected = 2
   actual   = 0
 }
 
 module "assert-strict-less" {
-  source   = "git::https://github.com/CodyKochmann/terraform-assert.git//modules/lt"
+  source   = "github.com/CodyKochmann/terraform-assert/lt"
   expected = 1
   actual   = 0
 }
 
 module "assert-match-regex" {
-  source   = "git::https://github.com/CodyKochmann/terraform-assert.git//modules/match"
+  source   = "github.com/CodyKochmann/terraform-assert/match"
   regex  = "[a-z]*"
   actual = "rego"
 }
